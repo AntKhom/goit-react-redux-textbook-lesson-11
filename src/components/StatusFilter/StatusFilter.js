05d1b7b2ea28e3a7e678207d4390137ee0970e7e
AntKhom <antonhomenko1988@gmail.com>
2023-11-14 18:13:53 -0500

    store

--- src/components/StatusFilter/StatusFilter.js
+++ src/components/StatusFilter/StatusFilter.js
@@ -1,12 +1,34 @@
+// Імпортуємо хук
+import { useSelector } from "react-redux";
+// Імпортуємо об'єкт значень фільтра
+import { statusFilters } from "../../redux/constants";
 import { Button } from "components/Button/Button";
-import css from "./StatusFilter.module.css";
+import css from "./StatusFilter.module.css"
+import { getStatusFilter } from "redux/selectors";
 
 export const StatusFilter = () => {
+  // Отримуємо значення фільтра із стану Redux
+  const filter = useSelector(getStatusFilter);
+
   return (
     <div className={css.wrapper}>
-      <Button>All</Button>
-      <Button>Active</Button>
-      <Button>Completed</Button>
+      <Button selected={filter === statusFilters.all}>All</Button>
+      <Button selected={filter === statusFilters.active}>Active</Button>
+      <Button selected={filter === statusFilters.completed}>Completed</Button>
     </div>
   );
 };
+
+
+// import { Button } from "components/Button/Button";
+// import css from "./StatusFilter.module.css";
+
+// export const StatusFilter = () => {
+//   return (
+//     <div className={css.wrapper}>
+//       <Button>All</Button>
+//       <Button>Active</Button>
+//       <Button>Completed</Button>
+//     </div>
+//   );
+// };
